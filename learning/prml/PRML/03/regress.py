ee7472bc6edbbda40cbd74b4667897e739c99be2
Jing Li <lijing@example.com>
2015-10-20 17:00:46 +0800

    add file

--- learning/prml/PRML/03/regress.py
+++ learning/prml/PRML/03/regress.py
@@ -1,11 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Polynomial regression: extending linear models with basis functions
+# http://scikit-learn.org/stable/modules/linear_model.html
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn import linear_model
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import Pipeline
 
-n = 10
-x = np.linspace(0, 2*np.pi, n)
-y = np.sin(x) + np.rand(n)
+degree = 6
+sigma = 0.1
+n = 100
+x = np.linspace(0, 3*np.pi, n)
+y = np.sin(x) + np.random.normal(0, sigma, n)
+X = x.reshape(x.size,1)
 
-clf = linear_model.BayesianRidge()
-clf.fit(x, y)
+model = Pipeline([('poly', PolynomialFeatures(degree=degree)),
+                  ('linear', linear_model.LinearRegression(fit_intercept=False))])
+model = model.fit(X, y)
+print model.named_steps['linear'].coef_
+
+# Plot outputs
+plt.plot(x, np.sin(x), 'b', label="sin(x)")
+plt.scatter(x, y, label="training points")
+plt.plot(X, model.predict(X), 'g-', label="polynomial of degree %d" % degree)
+plt.legend(loc='lower left')
+plt.show()
