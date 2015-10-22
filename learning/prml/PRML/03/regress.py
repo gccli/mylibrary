72830dd031c7dccbf889b6459300c08f8ea60de4
Jing Li <lijing@example.com>
2015-10-22 18:20:27 +0800

    regress

--- learning/prml/PRML/03/regress.py
+++ learning/prml/PRML/03/regress.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.matlib as matrix
 import matplotlib.pyplot as plt
 
 # Polynomial regression: extending linear models with basis functions
@@ -38,6 +39,51 @@ plt.plot(xpred, model.predict(xpred), label="polynomial^%d" % degree)
 xpred = np.linspace(0, (m+1)*np.pi, n)
 xpred = xpred.reshape(xpred.size, 1)
 plt.plot(xpred, svr.predict(xpred), 'g-', label="svr")
+
+
+
+# Bayesian method
+def phi(x):
+    D = degree + 1
+    Phi = np.matlib.zeros((D, 1))
+    for i in range(D):
+        Phi[i] = pow(x,i)
+    return Phi
+
+alpha = 5e-3
+beta = 11.1
+S = np.matlib.zeros((8,8))
+for i in range(x.size):
+    phix = phi(float(x[i]))
+    S += alpha * np.identity(8) + beta * phix * phix.T
+S = np.linalg.inv(S)
+
+def var(x):
+    phix = phi(x)
+    sigma = 1/beta + phix.T * S * phix
+    return float(sigma)
+
+def mean(a):
+    t = np.matlib.zeros((8,1))
+    for i in range(x.size):
+        t += (y[i] * phi(x[i][0]))
+    r = beta * phi(a).T * S * t
+
+    return float(r)
+
+print var(np.pi)
+print mean(np.pi/4), mean(np.pi/2), mean(np.pi)
+
+def predict(x):
+    return np.random.normal(mean(x), var(x))
+
+myfunc = np.vectorize(predict)
+xpred = np.linspace(0, m*np.pi, n)
+xpred = xpred.reshape(xpred.size, 1)
+plt.plot(xpred, myfunc(xpred), 'c-', label="Bayesian")
+
+# End Bayesian method
+
 
 plt.legend(loc='upper right')
 plt.show()
