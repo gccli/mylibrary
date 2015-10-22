7da9c7edc7158a8a36d9f7409a26664fdb9b953c
Jing Li <lijing@example.com>
2015-10-22 18:44:09 +0800

    revise

--- learning/prml/PRML/03/regress.py
+++ learning/prml/PRML/03/regress.py
@@ -40,9 +40,8 @@ xpred = np.linspace(0, (m+1)*np.pi, n)
 xpred = xpred.reshape(xpred.size, 1)
 plt.plot(xpred, svr.predict(xpred), 'g-', label="svr")
 
-
-
 # Bayesian method
+alpha=5e-3; beta=11.1
 def phi(x):
     D = degree + 1
     Phi = np.matlib.zeros((D, 1))
@@ -50,40 +49,36 @@ def phi(x):
         Phi[i] = pow(x,i)
     return Phi
 
-alpha = 5e-3
-beta = 11.1
-S = np.matlib.zeros((8,8))
-for i in range(x.size):
-    phix = phi(float(x[i]))
-    S += alpha * np.identity(8) + beta * phix * phix.T
-S = np.linalg.inv(S)
+def fit(x, y):
+    S = np.matlib.zeros((8,8))
+    for i in range(x.size):
+        phix = phi(float(x[i]))
+        S += alpha * np.identity(8) + beta * phix * phix.T
+    S = np.linalg.inv(S) # (1.72)
+    return S
 
 def var(x):
     phix = phi(x)
-    sigma = 1/beta + phix.T * S * phix
+    sigma = 1/beta + phix.T * S * phix # (1.71)
     return float(sigma)
 
 def mean(a):
     t = np.matlib.zeros((8,1))
     for i in range(x.size):
-        t += (y[i] * phi(x[i][0]))
-    r = beta * phi(a).T * S * t
+        t += y[i] * phi(float(x[i]))
+    r = beta * phi(a).T * S * t # (1.70)
 
     return float(r)
-
-print var(np.pi)
-print mean(np.pi/4), mean(np.pi/2), mean(np.pi)
 
 def predict(x):
     return np.random.normal(mean(x), var(x))
 
 myfunc = np.vectorize(predict)
-xpred = np.linspace(0, m*np.pi, n)
-xpred = xpred.reshape(xpred.size, 1)
+xpred = np.linspace(0, m*np.pi, n).reshape(n,1)
+
+S = fit(x,y)
 plt.plot(xpred, myfunc(xpred), 'c-', label="Bayesian")
-
-# End Bayesian method
-
+# End
 
 plt.legend(loc='upper right')
 plt.show()
