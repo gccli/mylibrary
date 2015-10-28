787d9367e41a79af9d73303a47f071cd050f367d
Jing Li <lijing@example.com>
2015-10-28 14:10:10 +0800

    done

--- learning/prml/PRML/04/classify.py
+++ learning/prml/PRML/04/classify.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Logistic Regression for two-class classification implementation
+# Using Iterative reweighted least squares(迭代重加权最小平方) algorithm
 
 import time
 import numpy as np
@@ -18,6 +20,7 @@ sigma = [X[:,0].std(), X[:,1].std()]
 N,_ = X.shape
 M = 10
 
+# max iterations
 iterations = 20
 
 # (4.59)
@@ -25,7 +28,7 @@ def sigmoid(x):
     ret = 1/(1 + np.exp(-x))
     return ret
 
-# Gaussian/polynomial basis function
+# fixed polynomial basis function
 def phi(x):
     ret = np.matlib.ones((M, 1))
 
@@ -34,6 +37,7 @@ def phi(x):
     ret[3] = np.power(ret[1], 2)
     ret[4] = np.power(ret[2], 2)
     ret[5] = ret[1] * ret[2]
+
     ret[6] = np.power(ret[1], 3)
     ret[7] = np.power(ret[2], 3)
     ret[8] = ret[1] * ret[4]
@@ -50,7 +54,7 @@ def vpredict(X, w):
     cols,_ = X.shape
     ret = np.zeros((cols, 1))
     for i in range(cols):
-        ret[i] = round(predict(X[i], w))
+        ret[i] = predict(X[i], w)
 
     return ret
 
@@ -59,28 +63,29 @@ logreg.fit(X, t)
 
 numpy.set_printoptions(precision=2)
 
-plt.figure(1)
-plt.subplot(311)
-plt.scatter(X[:, 0], X[:, 1], c=t, edgecolors='k')
+h = .025
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
-plt.subplot(312)
-xx = np.linspace(-5, 5, 100)
-plt.plot(xx, sigmoid(xx), 'k-')
-plt.title('sigmoid(x)')
+plt.figure(1, figsize=(8, 6))
+plt.title('Logistic Regression for tow-class classification')
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
 
 plt.show(block=False)
 
-Phi = np.matlib.zeros((N, M))
+PHI = np.matlib.zeros((N, M)) # (3.16) design matrix
 for i in range(N):
-    Phi[i] = phi(X[i]).T
-print 'Φ:\n',Phi,'\n--------\n',Phi.shape
+    PHI[i] = phi(X[i]).T
 
 w = np.matlib.zeros((M, 1))
-
+R = np.matlib.eye(N, dtype=float)
+y = np.matlib.zeros((N, 1))
+z = np.matlib.zeros((N, 1))
 for j in range(iterations):
-    R = np.matlib.eye(N, dtype=float)
-    y = np.matlib.zeros((N, 1))
-    z = np.matlib.zeros((N, 1))
     for i in range(N):
         y_n = predict(X[i], w)
         R[i,i] = y_n * (1 - y_n)
@@ -91,30 +96,24 @@ for j in range(iterations):
         print 'Failed to inverse R:\n',R
         R.tofile('R.txt', ',')
         break
-    z = Phi * w - Rinv * (y - np.mat(t).T)    # (4.100)
-    H = Phi.T * R * Phi                       # (4.97)
+
+    z = PHI * w - Rinv * (y - np.mat(t).T)    # (4.100)
+    H = PHI.T * R * PHI                       # (4.97)     # Hessian matrix
     try:
-        w_new = linalg.inv(H) * Phi.T * R * z # (4.99)
+        w_new = linalg.inv(H) * PHI.T * R * z # (4.99)
         w = w_new
         print 'w^new:', w_new.A1
     except:
         print 'Failed to inverse H:\n',H
         break;
 
-    plt.subplot(313)
-
-    h = .02
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-
     Z = vpredict(np.c_[xx.ravel(), yy.ravel()], w)
     Z = Z.reshape(xx.shape)
     plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
     plt.scatter(X[:, 0], X[:, 1], c=t, edgecolors='k')
-
     plt.draw()
 
+# verify data use original data and third party function
 for i in range(N):
     print 'p(C1|φ): %.2f' % predict(X[i], w), \
         'original t_n:', t[i], 'logreg', logreg.predict(X[i])
