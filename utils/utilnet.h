78900a54da54cee5cb30d119871c482a1c613a3c
Jing Li <lijing@example.com>
2016-01-14 20:02:18 +0800

    move common utils to other project

--- utils/utilnet.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_NET_H_
-#define UTIL_NET_H_
-
-extern int util_get_ipaddr(const char* ifname, char * ipaddr);
-extern int util_get_hwaddr(const char* ifname, char* hwaddr);
-extern int util_set_ifpromisc(const char* ifname, bool enable);
-
-#endif
