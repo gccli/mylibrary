72f51bdacde2cde39947c314f5f82501571d145a
Jing Li <lijing@example.com>
2016-01-15 10:22:36 +0800

    some change

--- projects/nginx/iptables.sh
+++ projects/nginx/iptables.sh
@@ -10,5 +10,16 @@ iptables -t nat -F
 iptables -F
 
 #configuration nat
+# --src 192.168.30.0/0
+
+iptables -A INPUT -d 10.0.0.0/8 -j DROP
+iptables -A OUTPUT -d 10.0.0.0/8 -j DROP
+
+# don't disturb
+iptables -t nat -d 10.0.0.0/8 -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-port 10001
+
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-port 8080
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-port 443
+
+iptables -L -n
+iptables -t nat -L -n
