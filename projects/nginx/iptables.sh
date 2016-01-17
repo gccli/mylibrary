701a19aa2b9d529d8ae313fe7fde06038441e4ba
Jing Li <lijing@example.com>
2016-01-17 22:53:52 +0800

    iptables

--- projects/nginx/iptables.sh
+++ projects/nginx/iptables.sh
@@ -4,6 +4,8 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 #echo "1" > /proc/sys/net/bridge/bridge-nf-call-iptables
 
+# Interested destination ip address
+DEST=140.207.202.226
 
 #drop nat
 iptables -t nat -F
@@ -19,8 +21,11 @@ iptables -F
 #iptables -t nat -d 10.0.0.0/8 -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-port 10001
 
 # only redirect specific dest ip, e.g. 106.120.160.78 for yunpan
-iptables -t nat -d 42.236.9.68 -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-port 8080
-# iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-port 8080
+if [ -n "$DEST" ]; then
+    iptables -t nat -A PREROUTING -p tcp -m tcp -d $DEST --dport 80 -j REDIRECT --to-port 8080
+else
+    iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-port 8080
+fi
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-port 443
 iptables -L PREROUTING -t nat -n
 
